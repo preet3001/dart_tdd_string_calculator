2f6a43581273f576f8ff3d63b6c7d81a2abf700a
Preet Singh <preetsingh3001@gmail.com>
2025-02-11 20:43:24 +0530

    green: handling new line as seprator

--- lib/dart_tdd_string_calculator.dart
+++ lib/dart_tdd_string_calculator.dart
@@ -2,6 +2,8 @@ int add(String number) {
   if (number.isEmpty) {
     return 0;
   }
-  final numbers = number.split(',').map((e) => int.tryParse(e) ?? 0).toList();
+  final seprator = ',|/n';
+  final numbers =
+      number.split(RegExp(seprator)).map((e) => int.tryParse(e) ?? 0).toList();
   return numbers.reduce((a, b) => a + b);
 }
