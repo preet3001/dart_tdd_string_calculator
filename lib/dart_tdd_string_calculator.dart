fa431296196e6d8708c64881124dcdf4bf4162b1
Preet Singh <preetsingh3001@gmail.com>
2025-02-11 20:51:10 +0530

    green: handling custom delimiters

--- lib/dart_tdd_string_calculator.dart
+++ lib/dart_tdd_string_calculator.dart
@@ -2,7 +2,11 @@ int add(String number) {
   if (number.isEmpty) {
     return 0;
   }
-  final seprator = ',|/n';
+  String seprator = ',|/n';
+  if (number.startsWith('//')) {
+    final parts = number.split('\n');
+    seprator = parts.first.substring(2);
+  }
   final numbers =
       number.split(RegExp(seprator)).map((e) => int.tryParse(e) ?? 0).toList();
   return numbers.reduce((a, b) => a + b);
