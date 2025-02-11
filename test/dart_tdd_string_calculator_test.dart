9d847eee06a819cc3dd1a01ea3360542ab678d59
Preet Singh <preetsingh3001@gmail.com>
2025-02-11 20:17:16 +0530

    red: add failing test for empty case

--- test/dart_tdd_string_calculator_test.dart
+++ test/dart_tdd_string_calculator_test.dart
@@ -2,7 +2,7 @@ import 'package:dart_tdd_string_calculator/dart_tdd_string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(add(''), 42);
+  test('should get 0 in case of empty string', () {
+    expect(add(''), 0);
   });
 }
