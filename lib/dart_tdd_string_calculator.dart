6d6e3a5d5be81e62734349c5786760e27863cb89
Preet Singh <preetsingh3001@gmail.com>
2025-02-11 21:03:43 +0530

    refactor: removed white spaces

--- lib/dart_tdd_string_calculator.dart
+++ lib/dart_tdd_string_calculator.dart
@@ -9,11 +9,9 @@ int add(String number) {
   }
   final numbers =
       number.split(RegExp(seprator)).map((e) => int.tryParse(e) ?? 0).toList();
-
   final negatives = numbers.where((n) => n < 0).toList();
   if (negatives.isNotEmpty) {
     throw Exception('negative numbers not allowed: ${negatives.join(", ")}');
   }
-
   return numbers.reduce((a, b) => a + b);
 }
